3f20086866883ac21b91104944723a3eb2c4fc31
Rubensalves <rubensalourenco@gmail.com>
2024-10-21 17:58:26 -0300

    Criando servidor http , criando Api rest , fastify

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-import {createServer} from 'node:http'
+/*import {createServer} from 'node:http'
 
 const server = createServer((req, res) =>{
-    console.log('oi');
+res.write("Hello world")
+
 
     return res.end()
     
 })
 
 server.listen(3002)
+*/
+
+import { fastify } from "fastify";
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello world'
+})
+
+server.get('/hello', () => {
+    return 'Hello Rocketseat'
+})
+
+server.get('/node', () => {
+    return 'Hello Node'
+})
+
+server.listen({
+    port: 3333
+})
